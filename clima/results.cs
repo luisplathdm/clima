7c31feec81aeddaa0dfb3b2e838ef05df0ec9d4a
clima <luisgustavoplathdisaro@gmail.com>
2024-04-16 11:52:42 -0300

    colocando os atributos e primeira frame

---
+++ clima/results.cs
@@ -0,0 +1,15 @@
+namespace clima;
+public class Results { 
+    public int temp {get; set;} 
+    public string description {get; set;} 
+    public string city {get; set;} 
+    public int humidity {get; set;} 
+    public double rain {get; set;} 
+    public string sunrise {get; set;} 
+    public string sunset {get; set;} 
+    public string wind_speedy {get; set;} 
+    public int wind_direction {get; set;} 
+    public string moon_phase {get; set;} 
+    public string img_id {get; set;} 
+    public string condition_code {get; set;} 
+    public string currently {get; set;} }
